d58bbf7ead530c1a7a20fe5b6cc9c5bc276f3fd8
Lestari Thomas <lestari@theguywhousedtoworkat.com>
2019-04-01 22:27:25 +0800

    Skeleton VCU support

--- contracts/VoluntaryCarbonUnit.sol
+++ contracts/VoluntaryCarbonUnit.sol
@@ -1,10 +1,51 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.7;
+pragma experimental ABIEncoderV2; // Before launch, replace if V2 not production 
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
  
 contract VoluntaryCarbonUnit is ERC721Full, ERC721Mintable {
+
+    // Long-term, most if not all, of this should be in IPFS JSON
+    // but staying on-chain makes things easy for now.
+    // Also if anything is left on-chain, a more generic structure should be
+    // used, in the style of EternalStorage contracts. 
+    struct VcuDetail {
+        uint32 id;
+
+        uint256 issuanceDate; // Maybe as days?
+        uint256 retirementDate; // Maybe as days?
+
+        uint256 vintageStart;
+        uint256 vintageEnd; // Is the end always known?
+
+        string name;
+        uint16 countryCodeNumeric; // See https://en.wikipedia.org/wiki/ISO_3166-1_numeric
+        uint16 sectoryScope; // Turn me into an enum
+        string methodology; // Are these actually numbers?
+        uint64 totalVintageQuantity; // What is the actual range here?
+        uint64 quantityIssued;
+        string[] additionalCertifications; // More structure???
+    }
+
+    mapping(uint32 => VcuDetail) public vcuDetails;
+
     // solhint-disable-next-line no-empty-blocks
     constructor() public ERC721Full("Voluntary Carbon Unit Certificates", "VCU") {
+    }
+
+    // solhint-disable-next-line no-unused-vars
+    function mint(address to, uint256 tokenId) public returns (bool) {
+        revert("Not implemented. Use mint(to, VcuDetail) instead.");
+    }
+
+    function mintVcu(address to, VcuDetail memory detail) public returns (bool) {
+        vcuDetails[detail.id] = detail;
+
+        require(0 == detail.retirementDate || detail.issuanceDate <= detail.retirementDate, 'Retirement must be after issuance.');
+        require(detail.vintageStart <= detail.vintageStart, "Vintage must end after start.");
+
+        _mint(to, detail.id);
+        return true;
     }
 }
--- test/VoluntaryCarbonUnitTest.js
+++ test/VoluntaryCarbonUnitTest.js
@@ -1,3 +1,5 @@
+import shouldFail from 'openzeppelin-test-helpers/src/shouldFail';
+
 const VoluntaryCarbonUnit = artifacts.require('VoluntaryCarbonUnit');
 
 contract('VoluntaryCarbonUnit', (accounts) => {
@@ -7,8 +9,32 @@ contract('VoluntaryCarbonUnit', (accounts) => {
     vcu = await VoluntaryCarbonUnit.deployed();
   });
 
+  it('cannot make a VCU without details', async () => {
+    await shouldFail(vcu.mint(accounts[0], 1));
+  });
+
   it('makes an NFT and moves it', async () => {
-    await vcu.mint(accounts[0], 1);
+    const details = {
+      id: 1,
+      name: 'Rick Sanchez',
+
+      issuanceDate: Date.now,
+      retirementDate: 0,
+
+      vintageStart: Date.now(),
+      vintageEnd: Date.now() + (90 * 24 * 60 * 60 * 1000),
+
+      methodology: 'A method',
+
+      countryCodeNumeric: 2,
+      sectoryScope: 14,
+      totalVintageQuantity: 1000,
+      quantityIssued: 120,
+
+      additionalCertifications: [],
+    };
+
+    await vcu.mintVcu(accounts[0], details);
     await vcu.safeTransferFrom(accounts[0], accounts[1], 1);
     assert.equal(accounts[1], await vcu.ownerOf.call(1));
   });
