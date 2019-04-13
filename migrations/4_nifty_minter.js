fa077ce7b1ebb1de113e444af67ff4b510cf5234
gichiba <griffin@colony.io>
2019-04-13 15:09:53 +0800

    Add another minter to 4_nifty_minter.js

--- migrations/4_nifty_minter.js
+++ migrations/4_nifty_minter.js
@@ -4,4 +4,5 @@ const VoluntaryCarbonUnit = artifacts.require('./VoluntaryCarbonUnit.sol');
 module.exports = async (deployer) => {
   const vcu = await VoluntaryCarbonUnit.deployed();
   await vcu.addMinter('0xa24491d6378e843D7fe84bf20C589cFc5aC77562');
+  await vcu.addMinter('0x6D758C3A656869bbb9431b6c67E500d24a838959');
 };
