baae282cdedc75a0e345ccf8c487cba9e0b4e952
gichiba <griffin@colony.io>
2019-04-13 15:39:36 +0800

    Add start:chain script and update migration This is in the interest of making local development easier to automate.
    The `start:chain` script runs /scripts/start_blockchain.sh, which starts
    a local instance of ganache with keys pre-defined in
    ganache-accounts.json

--- migrations/4_nifty_minter.js
+++ migrations/4_nifty_minter.js
@@ -3,6 +3,5 @@ const VoluntaryCarbonUnit = artifacts.require('./VoluntaryCarbonUnit.sol');
 // eslint-disable-next-line no-unused-vars
 module.exports = async (deployer) => {
   const vcu = await VoluntaryCarbonUnit.deployed();
-  await vcu.addMinter('0xa24491d6378e843D7fe84bf20C589cFc5aC77562');
-  await vcu.addMinter('0x6D758C3A656869bbb9431b6c67E500d24a838959');
+  await vcu.addMinter('0xb77d57f4959eafa0339424b83fcfaf9c15407461');
 };
