7d027020733e7a05a4a2c0fb62e77e71ea08296c
gichiba <griffin@colony.io>
2019-04-16 16:28:48 +0800

    Change `addMinter` account to (2)

--- migrations/4_nifty_minter.js
+++ migrations/4_nifty_minter.js
@@ -3,5 +3,5 @@ const VoluntaryCarbonUnit = artifacts.require('./VoluntaryCarbonUnit.sol');
 // eslint-disable-next-line no-unused-vars
 module.exports = async (deployer) => {
   const vcu = await VoluntaryCarbonUnit.deployed();
-  await vcu.addMinter('0xb77d57f4959eafa0339424b83fcfaf9c15407461');
+  await vcu.addMinter('0x9df24e73f40b2a911eb254a8825103723e13209c');
 };
