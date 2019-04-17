5dabc324b525ff466e3bd94e7d335cb2f18f568e
Lestari Thomas <lestari@theguywhousedtoworkat.com>
2019-04-17 18:31:46 +0200

    Don't forget my Metamask address

--- migrations/4_nifty_minter.js
+++ migrations/4_nifty_minter.js
@@ -5,6 +5,7 @@ const VoluntaryCarbonUnit = artifacts.require('./VoluntaryCarbonUnit.sol');
 const ADDRESS_OF_FIRST_MINTERS = [
   '0x9df24e73f40b2a911eb254a8825103723e13209c',
   '0x6D758C3A656869bbb9431b6c67E500d24a838959',
+  '0xa24491d6378e843D7fe84bf20C589cFc5aC77562', // Thomas Metamask
 ];
 
 // eslint-disable-next-line no-unused-vars
